5cc5812ec6f10a249e05fd9f8e90841a8c87bc2d
Yagosouzag <yagomatheuscecilia@gmail.com>
2022-04-04 23:24:21 -0300

    Projeto Finalizado

---
+++ src/controllers/PokemonController.js
@@ -0,0 +1,118 @@
+const res = require('express/lib/response');
+const Pokemon = require('../models/Pokemon');
+
+let message = "";
+let type = "";
+
+const getAll = async (req,res) => {
+    try {
+        setTimeout(() =>{
+            message = ""
+            type = ""
+        }, 1000)
+        
+        const pokedex = await Pokemon.findAll({ order: [["id","ASC"]]});
+        res.render("index", {pokedex, 
+            pokemonPut: null,
+            pokemonDel: null,
+            message, 
+            type, 
+        });
+    }
+    catch (err) {
+        res.status(500).send({err: err.message});
+    }
+};
+
+const signup = (req,res) => {
+    try {
+        res.render("signup", {message, type});
+    }
+    catch (err) {
+        res.status(500).send({err: err.message});
+    }
+
+};
+
+const create = async (req,res) => {
+    try {
+        const pokemon = req.body;
+
+        if(!pokemon.nome || !pokemon.descricao || !pokemon.tipo || !pokemon.imagem) {
+            message = "Preencha todos os campos para cadastrar!"
+            type = "danger"
+            return res.redirect("/signup");
+        }
+
+        await Pokemon.create(pokemon);
+        message = "Pokemon cadastrado com sucesso"
+        type = "success"
+        res.redirect("/")
+    }
+    catch (err) {
+        res.status(500).send({err: err.message});
+    }
+};
+
+const getById = async (req,res) => {
+    try {
+        const method = req.params.method;
+        const pokedex = await Pokemon.findAll({ order: [["id","ASC"]]});
+        const pokemon = await Pokemon.findByPk(req.params.id);
+
+        if (method == 'put') {
+            res.render("index", {
+                pokedex,
+                pokemonPut: pokemon,
+                pokemonDel: null,
+                message, 
+                type
+            });
+        } else {
+            res.render("index", {
+                pokedex,
+                pokemonPut: null,
+                pokemonDel: pokemon,
+                message,
+                type,
+            });
+        }
+    }
+    catch (err) {
+        res.status(500).send({err: err.message})
+    }
+};
+
+const update = async (req,res) => {
+    try {
+        const pokemon = req.body;
+        await Pokemon.update(pokemon, { where: { id: req.params.id }});
+        message = "Pokemon atualizado com sucesso"
+        type = "sucess"
+        res.redirect("/");
+    }
+    catch (err) {
+        res.status(500).send({err: err.message});
+    }
+};
+
+const remove = async (req,res) => {
+    try{
+        await Pokemon.destroy({where: {id: req.params.id}});
+        message = "Pokemon removido com sucesso"
+        type = "sucess"
+        res.redirect("/");
+    }
+    catch (err) {
+        res.status(500).send({err: err.message});
+    }
+};
+
+module.exports = {
+    getAll,
+    signup,
+    create,
+    getById,
+    update,
+    remove,
+};
